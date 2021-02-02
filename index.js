8fc613d399be21e4d5ac788999c39fac9550cc46
Nilo Almeida <niloalmeidadev@gmail.com>
2021-02-02 09:42:43 -0300

    teste

---
+++ index.js
@@ -0,0 +1,13 @@
+const DEFAULT_LANGUAGE = 'pt-br'
+function i18n(req){
+    const headers = req.headers["accept-language"].toLowerCase()
+    try{
+        let language = require(path.resolve(__dirname, `./languages/${headers}.json`))
+        return language
+    }catch(e){
+        let language = require(path.resolve(__dirname, `./languages/${DEFAULT_LANGUAGE}.json`))
+        return language
+    }
+}
+
+export { foo, bar };
