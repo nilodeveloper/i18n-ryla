719827f9839edebcee8e224ce5d2d216d1d8f223
Nilo Almeida <niloalmeidadev@gmail.com>
2021-02-02 15:57:25 -0300

    teste

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+const testFolder = './languages';
+const fs = require('fs');
+
  function getHeadersLanguages(headers){
-    let lang = headers.split(",");
-    lang = lang.map(lang.split(';')[0])
-    console.log(lang)
+    let header = headers.split(",");
+    let languages = []
+    let names = []
+    
+    fs.readdirSync(testFolder).forEach(file => {
+      names.push(file.split(".")[0])
+    });
+
+    header.forEach(lang => {
+      if(lang){
+        language = lang.toLowerCase()
+        languages.push(language.split(";")[0])
+      }
+    })
+    console.log("languages", languages)
+
+    for(let i=0;i<languages.length;i++){
+      if(names.includes(languages[i])){
+        return languages[i]
+      }
+    }
  }
  
  function i18n(req){
   try{
     const headers = getHeadersLanguages(req.headers["accept-language"])
-    console.log(headers)
+    console.log('headers', headers)
     let language = require(`../../languages/${headers}.json`)
     return language
   }catch(e){
