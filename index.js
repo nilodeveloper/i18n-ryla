2cff1e5afcdf9c6c3187e3ccacb1d47aa96c54f0
Nilo Almeida <niloalmeidadev@gmail.com>
2021-02-02 11:26:03 -0300

    teste

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
  function i18n(req){
   try{
-    const headers = (req.headers["accept-language"]).toString().toLowerCase()
+    const headers = (req.headers["accept-language"])
+    .toString()
+    .toLowerCase()
+    .replace(/["]/g, "");
     let language = require(`../../languages/${headers}.json`)
     return language
   }catch(e){
